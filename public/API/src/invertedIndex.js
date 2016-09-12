a0b20bc1b18c668d3097b856047cbd8ab9d5b455
codemuhammed <codemuhammed@gmail.com>
2016-09-12 18:07:38 +0100

    Scaffolded inverted-index app

---
+++ public/API/src/invertedIndex.js
@@ -0,0 +1,1 @@
+//@TODO inverted index API goes here
---
+++ tests/unit/invertedIndexTest.js
@@ -0,0 +1,8 @@
+/*
+* This function runs the tests for the application files
+*/
+(function() {
+  it('true is true', function(){
+    expect(true).toEqual(true);
+  });
+})();
